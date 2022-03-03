ac63dc39afc387f4afa7aba422406f8c73f631ea
Mikael Moilanen <mikael.moilanen@gmail.com>
2022-03-03 08:26:15 +0200

    Pass app to http server

--- osa4/index.js
+++ osa4/index.js
@@ -1,6 +1,9 @@
 const app = require("./app")
 const config = require("./utils/config")
+const http = require("http")
 
-app.listen(config.PORT, () => {
+const server = http.createServer(app)
+
+server.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
 })
