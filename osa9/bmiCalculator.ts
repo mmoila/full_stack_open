61c14c77aab021ca552aaceccf4536abbd273ddf
Mikael Moilanen <mikael.moilanen@gmail.com>
2022-08-24 08:46:32 +0300

    Exercises 9.1-9.3

---
+++ osa9/bmiCalculator.ts
@@ -0,0 +1,47 @@
+interface BmiValues {
+  height: number;
+  weight: number;
+}
+
+const parseArguments = (args: Array<string>): BmiValues => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+  if (args.length > 4) throw new Error("Too many arguments");
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3]),
+    };
+  } else {
+    throw new Error("Provided values were not numbers!");
+  }
+};
+
+const calculateBmi = (height: number, weight: number) => {
+  if (weight === 0) {
+    throw new Error("Weight cannot be zero!");
+  }
+  const bmi = Math.round((weight / (height / 100) ** 2) * 10) / 10;
+  if (bmi < 18.5) {
+    return "Underweight (Unhealthy weight)";
+  } else if (bmi >= 18.5 && bmi <= 22.9) {
+    return "Normal range (Healthy weight)";
+  } else if (bmi > 22.9 && bmi <= 24.9) {
+    return "Overweight I (At risk)";
+  } else if (bmi > 24.9 && bmi <= 29.9) {
+    return "Overweight II (Moderately obese)";
+  } else if (bmi > 29.9) {
+    return "Overweight III (Severely obese)";
+  }
+};
+
+try {
+  const { height, weight } = parseArguments(process.argv);
+  console.log(calculateBmi(height, weight));
+} catch (error: unknown) {
+  let errorMessage = "Something went wrong";
+  if (error instanceof Error) {
+    errorMessage.concat(`Error: ${error.message}`);
+  }
+  console.log(errorMessage);
+}
---
+++ osa9/exerciseCalculator.ts
@@ -0,0 +1,75 @@
+/*
+{ periodLength: 7,
+  trainingDays: 5,
+  success: false,
+  rating: 2,
+  ratingDescription: 'not too bad but could be better',
+  target: 2,
+  average: 1.9285714285714286 }
+*/
+
+interface ExerciseSummary {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+const parseList = (args: Array<string>): Array<number> => {
+  if (args.length < 3) throw new Error("Not enough arguments");
+
+  const data = args.slice(2)
+
+  const formattedData = data
+    .filter(n => !isNaN(Number(n)))
+    .map(n => Number(n))
+
+  if (data.length !== formattedData.length) {
+    throw new Error("Provided values were not numbers!");
+  }
+
+  return formattedData
+};
+
+const produceExerciseSummary = (exerciseHours: Array<number>): ExerciseSummary => {
+
+  const dailyTarget = 2
+  const totalHours = exerciseHours.reduce((prev, next) => prev + next)
+  const dailyAverage = totalHours / exerciseHours.length
+
+  const getRating = () => {
+    if (totalHours < 5) {
+      return {rating: 1, description: "poor"}
+    } else if (totalHours < 9) {
+      return {rating: 2, description: "average" }
+    } else {
+      return {rating: 3, description: "good" }
+    }
+  }
+
+  const ratingObject = getRating()
+
+  return {
+    periodLength: exerciseHours.length,
+    trainingDays: exerciseHours.filter(n => n !== 0).length,
+    success: dailyAverage >= dailyTarget ? true : false,
+    rating: ratingObject.rating,
+    ratingDescription: ratingObject.description,
+    target: dailyTarget,
+    average: dailyAverage,
+  };
+}
+
+try {
+  const exerciseList = parseList(process.argv)
+  console.log(produceExerciseSummary(exerciseList))
+} catch (error: unknown) {
+  let errorMessage = "Something went wrong";
+  if (error instanceof Error) {
+    errorMessage.concat(`Error: ${error.message}`);
+  }
+  console.log(errorMessage);
+}
